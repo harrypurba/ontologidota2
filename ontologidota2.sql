a1db9ec45ed740c8f978693a4b1506dc6a678be1
Harry Purba <harryoctavianuspurba@gmail.com>
2017-11-28 18:07:51 +0700

    Tambah kombinasi kelas, tambah mapping query dan tambah sql

---
+++ ontologidota2.sql
@@ -0,0 +1,47 @@
+create table herolist (
+	id INT,
+	nama TEXT,
+	atribut TEXT,
+	attackrange TEXT,
+	complexity TEXT,
+	carrypower TEXT,
+	supportpower TEXT
+);
+
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('1','abaddon','strength','melee','easy','weak','medium');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('2','alchemist','strength','melee','easy','medium','weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('3','axe','strength','melee','easy','very weak','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('4','beastmaster','strength','melee','medium','very weak','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('5','brewmaster','strength','melee','hard','weak','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('6','bristleback','strength','melee','easy','medium','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('7','centaur','strength','melee','easy','very weak','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('8','chaos knight','strength','melee','easy','strong','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('9','clockwerk','strength','melee','medium','very weak','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('10','doom','strength','melee','medium','weak','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('11','dragon knight','strength','melee','easy','medium','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('12','earth spirit','strength','melee','hard','very weak','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('13','earth shaker','strength','melee','medium','very weak','weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('14','elder titan','strength','melee','medium','very weak','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('15','huskar','strength','ranged','easy','medium','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('16','io','strength','ranged','hard','very weak','strong');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('17','kunkka','strength','melee','medium','weak','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('18','legion comander','strength','melee','easy','weak','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('19','lifestealer','strength','melee','medium','medium','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('20','lycan','strength','melee','medium','medium','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('21','magnus','strength','melee','medium','very weak','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('22','night stalker','strength','melee','easy','weak','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('23','omniknight','strength','melee','easy','very weak','medium');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('24','phoenix','strength','melee','medium','very weak','weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('25','pudge','strength','melee','medium','very weak','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('26','sand king','strength','melee','medium','very weak','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('27','slardar','strength','melee','easy','medium','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('28','spirit breaker','strength','melee','easy','weak','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('29','sven','strength','melee','easy','medium','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('30','tidehunter','strength','melee','easy','very weak','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('31','timbersaw','strength','melee','medium','very weak','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('32','tiny','strength','melee','easy','strong','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('33','treant protector','strength','melee','medium','very weak','strong');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('34','tusk','strength','melee','easy','very weak','very weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('35','underlord','strength','melee','medium','very weak','weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('36','undying','strength','melee','easy','very weak','weak');
+INSERT INTO herolist (id,nama,atribut,attackrange,complexity,carrypower,supportpower) VALUES ('37','wraith king','strength','melee','easy','medium','weak');
